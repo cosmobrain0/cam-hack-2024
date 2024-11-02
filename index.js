2fe4ad7b5a09a58dd5ce1acc2c0a567e63ee446b
Nickha007 <nickha007@gmail.com>
2024-11-02 12:17:23 +0000

    Rectangles on click!

--- index.js
+++ index.js
@@ -104,3 +104,24 @@ const selectRectangle = () => {
     let positionY = Math.random()*canvas.height;
     Composite.add(engine.world, Bodies.rectangle(positionX, positionY, 200, 50));
 };
+
+canvas.addEventListener('click', (event) => {
+    // Calculate position based on mouse click
+    const x = event.clientX;
+    const y = event.clientY;
+
+    // Create two new boxes at the click location, slightly offset from each other
+    const boxC = Bodies.rectangle(x - 40, y, 80, 80);
+    const boxD = Bodies.rectangle(x + 40, y, 80, 80);
+
+    // Create a rod (constraint) between the new boxes
+    const rod = Constraint.create({
+        length: 80,
+        stiffness: 0.9,
+        bodyA: boxC,
+        bodyB: boxD
+    });
+
+    // Add the new boxes and the rod to the world
+    Composite.add(engine.world, [boxC, boxD, rod]);
+});
