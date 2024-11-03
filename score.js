401b1256b6bbbe766c9efe96a7e815e3b7cd0f9e
cosmobrain0 <cosmobrain@outlook.com>
2024-11-03 16:22:49 +0000

    it remembers the high score

--- score.js
+++ score.js
@@ -1,4 +1,21 @@
 let score = 100000;
-let bestScore = 100000;
 let scoreIncrease = 30000;
-let scoreDecay = 0
+let scoreDecay = 0;
+let getHighScore = () => {
+  let result = parseInt(localStorage.getItem("highscore"));
+  if (isNaN(result)) {
+    setHighScore("highscore", "0");
+    return 0;
+  }
+  return result;
+}
+
+let setHighScore = v => {
+  localStorage.setItem("highscore", `${v}`);
+}
+
+let updateScore = v => {
+  if (v > getHighScore()) setHighScore(v);
+}
+
+updateScore(score);
