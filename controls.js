97348a53e9d2226fb6f4f6fab34ea669c89db7f0
Leo V <leov2006@gmail.com>
2024-11-03 15:08:02 +0000

    stabler ship

--- controls.js
+++ controls.js
@@ -11,9 +11,9 @@ function boost(body, amount) {
 }
 
 let keybinds = {
-    "a": () => applyRotate(ship, -0.04*shipParts),
-    "d": () => applyRotate(ship, 0.04*shipParts),
-    "w": () => boost(ship, 0.0002*shipParts),
+    "a": () => applyRotate(ship, -(0.1 + 0.02*shipParts)),
+    "d": () => applyRotate(ship, 0.1 + 0.02*shipParts),
+    "w": () => boost(ship, 0.0003*shipParts),
 }
 
 function smoothApply() {
